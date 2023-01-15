a070214fde6395fba43f450e2986f6a9a29641a0
ssolunna <ssolunna29@gmail.com>
2023-01-14 20:05:13 -0500

    Wrap characters from z to a

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -4,8 +4,10 @@ character = gets.chomp
 print "Type the shift factor: "
 shift_factor = (gets.chomp).to_i
 
-def shift_character(character, shift_factor)
-  (character.ord + shift_factor).chr
+def shift_lowercase_character(character, shift_factor)
+  range = ("a".."z").to_a
+  character_at_index = range.index(character)
+  range[(character_at_index + shift_factor) % 26]
 end
 
-puts "Caesar Cipher: #{shift_character(character, shift_factor)}"
+puts "Caesar Cipher: #{shift_lowercase_character(character, shift_factor)}"
