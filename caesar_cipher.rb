465871abcde1828c774e70d1b35fa165095e1ea1
ssolunna <ssolunna29@gmail.com>
2023-01-16 14:16:39 -0500

    Shift uppercase characters

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -4,10 +4,14 @@ character = gets.chomp
 print "Type the shift factor: "
 shift_factor = (gets.chomp).to_i
 
-def shift_lowercase_character(character, shift_factor)
-  range = ("a".."z").to_a
+def shift_character(character, shift_factor)
+  if character == character.downcase
+    range = ("a".."z").to_a
+  else
+    range = ("A".."Z").to_a
+  end  
   character_at_index = range.index(character)
   range[(character_at_index + shift_factor) % 26]
 end
 
-puts "Caesar Cipher: #{shift_lowercase_character(character, shift_factor)}"
+puts "Caesar Cipher: #{shift_character(character, shift_factor)}"
