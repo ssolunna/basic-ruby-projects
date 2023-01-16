a3fdbcf82378436f65cffdb3306cf9ed54ea55a0
ssolunna <ssolunna29@gmail.com>
2023-01-16 14:37:31 -0500

    Shift a whole string

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,17 +1,26 @@
-print "Type the character to be modified: "
-character = gets.chomp
+print "Type the string to be modified: "
+string = gets.chomp
 
 print "Type the shift factor: "
 shift_factor = (gets.chomp).to_i
+
+def caesar_cipher(string, shift_factor)
+  string_modified = Array.new
+  string.each_char do |c|
+    string_modified.push(shift_character(c, shift_factor))
+  end
+  string_modified.join
+end
 
 def shift_character(character, shift_factor)
   if character == character.downcase
     range = ("a".."z").to_a
   else
     range = ("A".."Z").to_a
-  end  
+  end
+
   character_at_index = range.index(character)
   range[(character_at_index + shift_factor) % 26]
 end
 
-puts "Caesar Cipher: #{shift_character(character, shift_factor)}"
+puts "Caesar Cipher: #{caesar_cipher(string, shift_factor)}"
