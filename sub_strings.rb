5c75ecdc423dca92450877bdfe913f2f439160a8
ssolunna <ssolunna29@gmail.com>
2023-01-16 17:51:48 -0500

    List and count the elements found in the string

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,11 +1,17 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
 def substrings(string, array)
-  matches = Array.new
+  matches = Hash.new(0)
+  string = string.split
 
   array.each do |element|
-    if string.match?(/#{element}/i)
-      matches.push(element)
+    for substring in string do
+      matches[element] += 1 if substring.match?(/#{element}/i)
     end
   end
 
   matches
 end
+
+puts substrings("Below", dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
