39bd1153f3a35a8262d43ceba077f16bc54d99b4
ssolunna <ssolunna29@gmail.com>
2023-03-27 18:13:39 -0500

    [fibonacci] Add method that returns fib sequence

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,1 +1,22 @@
+# frozen_string_literal: true
 
+# Fibonacci Sequence, using iteration
+def fibs(num)
+  return unless num.positive?
+
+  sequence = [0, 1]
+
+  return [0] if num == 1
+
+  return sequence if num == 2
+
+  i = 0
+  while sequence.length < num
+    sequence << sequence[i] + sequence[i + 1]
+    i += 1
+  end
+
+  sequence
+end
+
+p fibs(8)
