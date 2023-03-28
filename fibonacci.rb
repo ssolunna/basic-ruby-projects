1c165081878c6987dd319aa0cce7b750453fc203
ssolunna <ssolunna29@gmail.com>
2023-03-28 14:02:20 -0500

    [fibonacci] Return fib sequence using recursion

--- fibonacci.rb
+++ fibonacci.rb
@@ -19,4 +19,15 @@ def fibs(num)
   sequence
 end
 
+# Fibonacci Sequence, using recursion
+def fibs_rec(num, i = 0, sequence = [])
+  return sequence if sequence.length == num
+
+  sequence << ( i < 2 ? i : sequence[i - 1] + sequence[i - 2] )
+
+  fibs_rec(num, i += 1, sequence)
+end
+
 p fibs(8)
+puts
+p fibs_rec(8)
