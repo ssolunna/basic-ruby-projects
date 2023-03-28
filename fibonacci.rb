4823ac8a5aca1601f3e4a46c05eccb5fa2348969
ssolunna <ssolunna29@gmail.com>
2023-03-28 14:20:33 -0500

    [fibonacci] Refactor the method using iteration

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,19 +1,9 @@
 # frozen_string_literal: true
 
 # Fibonacci Sequence, using iteration
-def fibs(num)
-  return unless num.positive?
-
-  sequence = [0, 1]
-
-  return [0] if num == 1
-
-  return sequence if num == 2
-
-  i = 0
-  while sequence.length < num
-    sequence << sequence[i] + sequence[i + 1]
-    i += 1
+def fibs(num, sequence = [])
+  num.times do |i|
+    sequence << (i < 2 ? i : sequence[i - 1] + sequence[i - 2])
   end
 
   sequence
@@ -23,7 +13,7 @@ end
 def fibs_rec(num, i = 0, sequence = [])
   return sequence if sequence.length == num
 
-  sequence << ( i < 2 ? i : sequence[i - 1] + sequence[i - 2] )
+  sequence << (i < 2 ? i : sequence[i - 1] + sequence[i - 2])
 
   fibs_rec(num, i += 1, sequence)
 end
