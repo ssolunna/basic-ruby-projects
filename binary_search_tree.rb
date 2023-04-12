95dddf7a09451a707f2d4bf1d27691f0a3801677
ssolunna <ssolunna29@gmail.com>
2023-04-12 17:58:08 -0500

    [BST] Add method to insert a node in the tree

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -2,7 +2,10 @@
 
 # Nodes of the Tree
 class Node
-  def initialize(data, left, right)
+  attr_reader :data, :left, :right
+  protected attr_writer :data, :left, :right
+
+  def initialize(data, left = nil, right = nil)
     @data = data
     @left = left
     @right = right
@@ -10,12 +13,46 @@ class Node
 end
 
 # Binary Search Tree
-class Tree
+class Tree < Node
   attr_reader :array, :root
 
   def initialize(array)
     @array = array
     @root = build_tree(array.uniq.sort)
+  end
+
+  # Inserts a leaf Node in the Tree
+  def insert(value)
+    node = @root
+
+    return if @root.empty?
+
+    while node
+      if value == node.data
+        break
+      elsif value < node.data
+        if node.left.nil?
+          node.left = Node.new(value)
+          break
+        else
+          node = node.left
+        end
+      else
+        if node.right.nil?
+          node.right = Node.new(value)
+          break
+        else
+          node = node.right
+        end
+      end
+    end
+  end
+
+  # Pretty prints the Tree
+  def print(node = @root, prefix = '', is_left = true)
+    print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
 
