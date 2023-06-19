3149516685ac17537c8f431f92281eccf4785dbf
ssolunna <ssolunna29@gmail.com>
2023-06-19 06:30:21 -0500

    [KT] Create knight board of all possible moves

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,25 +1,23 @@
 # frozen_string_literal: true
 
 # Chess board
-board = [
-  %w[[0,7] [1,7] [2,7] [3,7] [4,7] [5,7] [6,7] [7,7]],
-  %w[[0,6] [1,6] [2,6] [3,6] [4,6] [5,6] [6,6] [7,6]],
-  %w[[0,5] [1,5] [2,5] [3,5] [4,5] [5,5] [6,5] [7,5]],
-  %w[[0,4] [1,4] [2,4] [3,4] [4,4] [5,4] [6,4] [7,4]],
-  %w[[0,3] [1,3] [2,3] [3,3] [4,3] [5,3] [6,3] [7,3]],
-  %w[[0,2] [1,2] [2,2] [3,2] [4,2] [5,2] [6,2] [7,2]],
-  %w[[0,1] [1,1] [2,1] [3,1] [4,1] [5,1] [6,1] [7,1]],
-  %w[[0,0] [1,0] [2,0] [3,0] [4,0] [5,0] [6,0] [7,0]]
-]
+#  [0,7] [1,7] [2,7] [3,7] [4,7] [5,7] [6,7] [7,7]
+#  [0,6] [1,6] [2,6] [3,6] [4,6] [5,6] [6,6] [7,6]
+#  [0,5] [1,5] [2,5] [3,5] [4,5] [5,5] [6,5] [7,5]
+#  [0,4] [1,4] [2,4] [3,4] [4,4] [5,4] [6,4] [7,4]
+#  [0,3] [1,3] [2,3] [3,3] [4,3] [5,3] [6,3] [7,3]
+#  [0,2] [1,2] [2,2] [3,2] [4,2] [5,2] [6,2] [7,2]
+#  [0,1] [1,1] [2,1] [3,1] [4,1] [5,1] [6,1] [7,1]
+#  [0,0] [1,0] [2,0] [3,0] [4,0] [5,0] [6,0] [7,0]
 
 def knight_moves(start_square, end_square)
-  knight = Tree.new(Node.new(start_square))
-  knight.travail
-  knight.root
+  knight_board = Board.new(Square.new(start_square))
+  knight_board.create
+  knight_board.board
 end
 
-# Knight next possible moves from starting square
-class Node
+# Knight next possible @moves from starting Square (@data)
+class Square
   attr_accessor :data, :moves
 
   def initialize(data)
@@ -28,26 +26,41 @@ class Node
   end
 end
 
-# Knight tree
-class Tree < Node
-  attr_reader :root
+# Knight @board of all possible moves
+class Board < Square
+  attr_reader :board
 
-  def initialize(root)
-    @root = root
+  def initialize(start)
+    @board = [start]
   end
 
-  # Gets all possible moves from starting point (square)
-  def travail(square = @root)
-    square.moves.push Node.new(right(square.data, 'up')),
-                      Node.new(right(square.data, 'down')),
-                      Node.new(left(square.data, 'up')),
-                      Node.new(left(square.data, 'down')),
-                      Node.new(up(square.data, 'right')),
-                      Node.new(up(square.data, 'left')),
-                      Node.new(down(square.data, 'right')),
-                      Node.new(down(square.data, 'left'))
+  # Creates @board with all possible moves the Knight can make
+  def create(queue = [*@board])
+    return if queue.empty?
 
-    square.moves = square.moves.reject { |move| move.data.nil? }
+    square = queue.first
+
+    square.moves.push right(square.data, 'up'),
+                      right(square.data, 'down'),
+                      left(square.data, 'up'),
+                      left(square.data, 'down'),
+                      up(square.data, 'right'),
+                      up(square.data, 'left'),
+                      down(square.data, 'right'),
+                      down(square.data, 'left')
+
+    square.moves.compact!
+
+    square.moves.each do |move|
+      next if @board.find { |node| node.data == move }
+
+      move = Square.new(move)
+      @board.push(move)
+      queue.push(move)
+    end
+
+    queue.shift
+    create(queue)
   end
 
   def right(current_square, side)
